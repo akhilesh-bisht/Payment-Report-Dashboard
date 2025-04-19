ebd99fed5dea32a2508f7e58664a8cea8825a8f2
Akhilesh Bisht <150502170+akhilesh-bisht@users.noreply.github.com>
2025-04-20 03:19:27 +0530

    add api

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:4500",
+        target: "https://payment-report-dashboard.onrender.com",
         changeOrigin: true,
         secure: false,
       },
